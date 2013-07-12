95af475c1af4d6712fef1467dc541ed4fd83f328
chadaharitha <chadaharitha@gmail.com>
2013-07-12 14:51:38 +0530

    My First App in github

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+JobBoard::Application.routes.draw do
+
+
+  resources :applyonlines
+  resources :jobdetails
+
+
+  #match "job_results/index/:pno" => "job_results#index", :as => :job_results_view
+
+  match "jobdetails" => "jobdetails#index", :as => :job_detail_view
+  match "applyonlines" => "applyonlines#index", :as => :job_apply_view  
+  match 'applyonlines/applyfinish' => 'applyonlines#applyfinish', :as => :applyfinish
+  match "job_results/index/:pno" => "job_results#index", :as => :index
+
+  root :to => 'job_results#index#1'
+
+
+  
+end
